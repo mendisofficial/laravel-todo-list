24fef791d53cdcc76e64c1786f84f3535b8ab8a0
Chathusha Mendis <86899589+mendisofficial@users.noreply.github.com>
2025-08-17 17:44:32 +0530

    learn: laravel blade directives

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,15 @@
-Hello. I'm a blade template.
-@isset($name)
-    <div>The name is {{ $name }}</div>
-@endisset
+<h1>The list of tasks</h1>
+
+<div>
+    {{-- @if (count($tasks)) --}}
+    @forelse ($tasks as $task)
+        <div>
+            <div>
+                <a href="{{ route('task.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+            </div>
+        </div>
+    @empty
+        <div>No tasks found</div>
+    @endforelse
+    {{-- @endif --}}
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,27 +2,85 @@
 
 use Illuminate\Support\Facades\Route;
 
+// This is how you define a class in PHP
+class Task
+{
+    public function __construct(
+        public int $id,
+        public string $title,
+        public string $description,
+        public ?string $long_description,
+        public bool $completed,
+        public string $created_at,
+        public string $updated_at
+    ) {}
+}
+
+$tasks = [
+    new Task(
+        1,
+        'Buy groceries',
+        'Task 1 description',
+        'Task 1 long description',
+        false,
+        '2025-03-01 12:00:00',
+        '2025-03-01 12:00:00'
+    ),
+    new Task(
+        2,
+        'Sell old stuff',
+        'Task 2 description',
+        null,
+        false,
+        '2025-03-02 12:00:00',
+        '2025-03-02 12:00:00'
+    ),
+    new Task(
+        3,
+        'Learn programming',
+        'Task 3 description',
+        'Task 3 long description',
+        true,
+        '2025-03-03 12:00:00',
+        '2025-03-03 12:00:00'
+    ),
+    new Task(
+        4,
+        'Take dogs for a walk',
+        'Task 4 description',
+        null,
+        false,
+        '2025-03-04 12:00:00',
+        '2025-03-04 12:00:00'
+    ),
+];
+
+
 // This route displays the welcome page with a view
-Route::get('/', function () {
+Route::get('/', function () use ($tasks) {
     return view('index', [
-        'name' => 'Chathusha Mendis'
+        'tasks' => $tasks
     ]);
-});
+})->name('task.index');
+
+Route::get('/{id}', function ($id) {
+    return "One single task";
+})->name('task.show');
 
 // This route returns a simple string response
-Route::get('/hello', function () {
-    return 'Hello, World!';
-})->name('hello');
+// Route::get('/hello', function () {
+//     return 'Hello, World!';
+// })->name('hello');
 
 // This route redirects to the hello route
-Route::get('hallo', function () {
-    return redirect()->route('hello');
-});
+// Route::get('hallo', function () {
+//     return redirect()->route('hello');
+// });
 
 // This is how dynamic route parameters work
-Route::get('greet/{name}', function ($name) {
-    return 'Hello, ' . $name . '!';
-});
+// Route::get('greet/{name}', function ($name) {
+//     return 'Hello, ' . $name . '!';
+// });
 
 // This is how fallback routes work
 Route::fallback(function () {
