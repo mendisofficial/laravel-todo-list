19d6b89b78ace4d8c73387cc7b62612e053c69ff
Chathusha Mendis <86899589+mendisofficial@users.noreply.github.com>
2025-08-17 19:39:41 +0530

    refactor: replace hardcoded tasks with dynamic model retrieval

--- routes/web.php
+++ routes/web.php
@@ -1,78 +1,24 @@
 <?php
 
+use App\Models\Task as ModelsTask;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
-
-// This is how you define a class in PHP
-class Task
-{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description,
-        public bool $completed,
-        public string $created_at,
-        public string $updated_at
-    ) {}
-}
-
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2025-03-01 12:00:00',
-        '2025-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2025-03-02 12:00:00',
-        '2025-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2025-03-03 12:00:00',
-        '2025-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2025-03-04 12:00:00',
-        '2025-03-04 12:00:00'
-    ),
-];
 
 Route::get('/', function () {
     return redirect()->route('task.index');
 });
 
 // This route displays the welcome page with a view
-Route::get('/tasks', function () use ($tasks) {
+Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => $tasks
+        'tasks' => ModelsTask::latest()->get()
     ]);
 })->name('task.index');
 
-Route::get('/tasks/{id}', function ($id) use ($tasks) {
-    $task = collect($tasks)->firstWhere('id', $id);
-    if (!$task) {
-        abort(Response::HTTP_NOT_FOUND);
-    }
-    return view('show', ['task' => $task]);
+Route::get('/tasks/{id}', function ($id) {
+    return view('show', [
+        'task' => ModelsTask::findOrFail($id)
+    ]);
 })->name('task.show');
 
 // This route returns a simple string response
